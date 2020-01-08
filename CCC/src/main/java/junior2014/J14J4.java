4c10ec09cfb17bfd571f7ae66171067f1114a376
Yan Ren <ryan.ren3@gmail.com>
2020-01-08 13:37:41 -0500

    add junior 2014 j4

---
+++ CCC/src/main/java/junior2014/J14J4.java
@@ -0,0 +1,31 @@
+package junior2014;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class J14J4 {
+	public ArrayList<Integer> run(BufferedReader br) {
+		Scanner sc = new Scanner(br);
+		int people = sc.nextInt();
+		ArrayList<Integer> peopleList = new ArrayList<>();
+		// add people 0 at the beginning
+		peopleList.add(0);
+		for(int i = 1; i <= people; i++) {
+			peopleList.add(i);
+		}
+		int m = sc.nextInt();
+		for(int i = 0; i < m; i++) {
+			int removeIndex = sc.nextInt();
+			int j = removeIndex;
+			while(j < peopleList.size()) {
+				peopleList.remove(j);
+				j -= 1;
+				j += removeIndex;				
+			}
+		}
+		// remove the leading 0
+		peopleList.remove(0);
+		return peopleList;
+	}
+}
