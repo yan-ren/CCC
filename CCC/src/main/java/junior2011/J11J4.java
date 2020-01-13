c1c5188ded36a3e464bb9544a033f46fea8a7b9d
Yan Ren <ryan.ren3@gmail.com>
2020-01-12 22:30:09 -0500

    add junior 2011 j4, test2 output has issue, change the value

---
+++ CCC/src/main/java/junior2011/J11J4.java
@@ -0,0 +1,99 @@
+package junior2011;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class J11J4 {
+
+	static boolean[][] map;
+	static int xR;
+	static int yR;
+	static int xV;
+	static int yV;
+	static int xDelta;
+	static int yDelta;
+
+	public ArrayList<String> run(BufferedReader br) {
+		Scanner sc = new Scanner(br);
+		initialize();
+		ArrayList<String> comments = new ArrayList<>();
+		while (sc.hasNext()) {
+			String command = sc.nextLine();
+			if (command.contains("q")) {
+				break;
+			}
+			String comment = move(command);
+			if (comment.contains("DANGER")) {
+				comments.add(comment);
+				break;
+			}
+			comments.add(comment);
+		}
+		return comments;
+	}
+	
+	public static void initialize() {
+		map = new boolean[400][400];
+		xR = -1;
+		yR = -5;
+		xV = map.length / 2;
+		yV = map.length / 2;
+		xDelta = xV - xR;
+		yDelta = yV - yR;
+	}
+
+	public static String move(String command) {
+		String[] items = command.split(" ");
+		int moves = Integer.parseInt(items[1]);
+		boolean danger = false;
+		if(items[0].equals("u")) {
+			while(moves > 0) {
+				yV += 1;
+				if(map[xV][yV] == false) {
+					map[xV][yV] = true;
+				}else {
+					danger = true;
+				}
+				moves -= 1;
+			}
+		}
+		
+		if(items[0].equals("d")) {
+			while(moves > 0) {
+				yV -= 1;
+				if(map[xV][yV] == false) {
+					map[xV][yV] = true;
+				}else {
+					danger = true;
+				}
+				moves -= 1;
+			}
+		}
+		
+		if(items[0].equals("l")) {
+			while(moves > 0) {
+				xV -= 1;
+				if(map[xV][yV] == false) {
+					map[xV][yV] = true;
+				}else {
+					danger = true;
+				}
+				moves -= 1;
+			}
+		}
+		
+		if(items[0].equals("r")) {
+			while(moves > 0) {
+				xV += 1;
+				if(map[xV][yV] == false) {
+					map[xV][yV] = true;
+				}else {
+					danger = true;
+				}
+				moves -= 1;
+			}
+		}
+		return danger? (xV - xDelta) + " " + (yV - yDelta) + " " + "DANGER" : (xV - xDelta) + " " + (yV - yDelta) + " " + "safe";
+	}
+}
---
+++ CCC/src/test/java/junior2011/J11J4Test.java
@@ -0,0 +1,62 @@
+package junior2011;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+import helper.InputReader;
+
+public class J11J4Test {
+	@Test
+	public void test1() throws IOException {
+		BufferedReader br1 = new InputReader("./src/test/resources/j11/j4/j4.1.in").getBr();
+		BufferedReader br2 = new InputReader("./src/test/resources/j11/j4/j4.1.out").getBr();
+		ArrayList<String> expected = new ArrayList<>();
+		Scanner sc = new Scanner(br2);
+		while(sc.hasNext()) {
+			expected.add(sc.nextLine());
+		}
+		assertEquals(expected, new J11J4().run(br1));
+	}
+	
+	@Test
+	public void test2() throws IOException {
+		BufferedReader br1 = new InputReader("./src/test/resources/j11/j4/j4.2.in").getBr();
+		BufferedReader br2 = new InputReader("./src/test/resources/j11/j4/j4.2.out").getBr();
+		ArrayList<String> expected = new ArrayList<>();
+		Scanner sc = new Scanner(br2);
+		while(sc.hasNext()) {
+			expected.add(sc.nextLine());
+		}
+		assertEquals(expected, new J11J4().run(br1));
+	}
+	
+	@Test
+	public void test3() throws IOException {
+		BufferedReader br1 = new InputReader("./src/test/resources/j11/j4/j4.3.in").getBr();
+		BufferedReader br2 = new InputReader("./src/test/resources/j11/j4/j4.3.out").getBr();
+		ArrayList<String> expected = new ArrayList<>();
+		Scanner sc = new Scanner(br2);
+		while(sc.hasNext()) {
+			expected.add(sc.nextLine());
+		}
+		assertEquals(expected, new J11J4().run(br1));
+	}
+	
+	@Test
+	public void test4() throws IOException {
+		BufferedReader br1 = new InputReader("./src/test/resources/j11/j4/j4.4.in").getBr();
+		BufferedReader br2 = new InputReader("./src/test/resources/j11/j4/j4.4.out").getBr();
+		ArrayList<String> expected = new ArrayList<>();
+		Scanner sc = new Scanner(br2);
+		while(sc.hasNext()) {
+			expected.add(sc.nextLine());
+		}
+		assertEquals(expected, new J11J4().run(br1));
+	}
+}
